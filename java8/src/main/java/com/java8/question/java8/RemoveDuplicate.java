3d39b2602c663ba9a9913102944b4c4c0a30571a
osihar <osihark73@gmail.com>
2026-01-07 23:54:24 +0530

    add java 8 features

--- java8/src/main/java/com/java8/question/java8/RemoveDuplicate.java
+++ java8/src/main/java/com/java8/question/java8/RemoveDuplicate.java
@@ -1,6 +1,7 @@
 package com.java8.question.java8;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,7 +14,24 @@ public class RemoveDuplicate {
 	        List<String> uniqueStrngs = listOfStrings.stream().distinct().collect(Collectors.toList());
 	         
 	        System.out.println(uniqueStrngs);
-		
+	        
+	        List<Integer> listOfIntegers = Arrays.asList(45, 12, 56, 15, 24, 75, 31, 89);
+	         
+	        listOfIntegers.stream().filter(i -> i % 5 == 0).forEach(System.out::println);
+	        
+	        
+	        //Given a list of integers, find maximum and minimum of those numbers?
+	        
+	        List<Integer> listOfInteger = Arrays.asList(45, 12, 56, 15, 24, 75, 31, 89);
+	         
+	        int max = listOfInteger.stream().max(Comparator.naturalOrder()).get();
+	         
+	        System.out.println("Maximum Element : "+max);
+	         
+	        int min = listOfInteger.stream().min(Comparator.naturalOrder()).get();
+	         
+	        System.out.println("Minimum Element : "+min);
+	    
 	}
 
 }
