abb12d2f3871bf19174e0af2e47f76c926509fb8
osihar <osihark73@gmail.com>
2026-01-18 03:36:49 +0530

    Print first 10 even number

--- java8/src/main/java/com/java8/question/FirstTenOddNumbers.java
+++ java8/src/main/java/com/java8/question/FirstTenOddNumbers.java
@@ -4,10 +4,16 @@ import java.util.stream.IntStream;
 
 public class FirstTenOddNumbers {
 
-	// Print the first 10 odd numbers
-
 	public static void main(String args[]) {
-		
+
+		// Print the first 10 odd numbers
+
 		IntStream.iterate(1, n -> n + 2).limit(10).forEach(System.out::println);
+
+		// Print the first 10 even numbers
+
+		IntStream.iterate(2, n -> n + 2).limit(10).forEach(System.out::println);
+
 	}
+
 }
